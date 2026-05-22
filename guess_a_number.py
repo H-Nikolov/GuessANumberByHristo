21a8c330c3d41345a431d6de224148cab9177868
Hristo Nikolov <hristaki2006@gmail.com>
2026-05-23 02:09:48 +0300

    Update guess_a_number.py
    Added colors

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,5 +1,10 @@
 import random
 
+GREEN = "\033[32m"
+RED = "\033[31m"
+BLUE = "\033[34m"
+RESET = "\033[0m"
+
 computer_number = random.randint(1, 100)
 while True:
     player_input = input("Guess the number (1-100:) ")
@@ -9,9 +14,9 @@ while True:
     player_number = int(player_input)
 
     if player_number == computer_number:
-        print("You guess it!")
+        print(f"{GREEN}You guess it!{RESET}")
         break
     elif player_number > computer_number:
-        print("Too High!")
+        print(f"{BLUE}Too High!{RESET}")
     else:
-        print("Too Low!")
+        print(f"{RED}Too Low!{RESET}")
